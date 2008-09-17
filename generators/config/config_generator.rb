cbed271bedc235e43f098c27947904c89a8d57a2
Cody Maggard <cody.maggard@gmail.com>
2008-09-17 09:51:41 -0500

    Parsing database.yml to generate config.yml environments.

--- generators/config/config_generator.rb
+++ generators/config/config_generator.rb
@@ -1,8 +1,13 @@
 class ConfigGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      m.file "config.yml", "config/config.yml"
+      #m.file "config.yml", "config/config.yml"
+      m.template "config.yml.erb", "config/config.yml", :assigns => { :env => get_environments }
       m.readme "INSTALL"
     end
+  end
+
+  def get_environments
+    YAML.load("config/database.yml").keys
   end
 end
