512051edb28f6b978674ecd24cdabd06aa7c7dad
Cody Maggard <cody.maggard@gmail.com>
2008-09-18 12:12:07 -0500

    Updated for configatron 2.1.1

--- generators/config/config_generator.rb
+++ generators/config/config_generator.rb
@@ -3,6 +3,7 @@ class ConfigGenerator < Rails::Generator::Base
     record do |m|
       #m.file "config.yml", "config/config.yml"
       m.template "config.yml.erb", "config/config.yml", :assigns => { :env => get_environments }
+      m.file "load_config.rb", "config/initializers/load_config.rb"
       m.readme "INSTALL"
     end
   end
---
+++ generators/config/templates/load_config.rb
@@ -0,0 +1,1 @@
+configatron.load_from_yaml("config/config.yml", :hash => Rails.env)
